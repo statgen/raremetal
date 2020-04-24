ac76eac5476d6f674280507d197d959d544fe19f
Ryan Welch <welchr@umich.edu>
2020-04-24 11:47:29 -0400

    Fix test cases; add test for -log10(p) on extreme small p-value
    Issue #23

--- raremetal/tests/tests.cpp
+++ raremetal/tests/tests.cpp
@@ -5,6 +5,53 @@
 #include "../src/GroupFromAnnotation.h"
 #include "../src/Meta.h"
 
+TEST_CASE("P-value precision") {
+  SECTION("Score statistic resulting in very small p-value") {
+    Meta meta;
+    String filename;
+
+    if (meta.prefix == "") {
+      filename = "raremetal.log";
+    }
+    else if (meta.prefix.Last() == '.' || meta.prefix.Last() == '/') {
+      filename = meta.prefix + "raremetal.log";
+    }
+    else {
+      filename = meta.prefix + ".raremetal.log";
+    }
+
+    FILE *logFile = freopen(filename, "wt", stderr);
+    meta.setLogFile(logFile);
+    meta.skipOutput = true;
+
+    meta.scorefile.Add("tests/raremetal/test_tut_rm/inputs/STUDY1.QT1.singlevar.score.txt.gz");
+    meta.scorefile.Add("tests/raremetal/test_tut_rm/inputs/STUDY2.QT1.singlevar.score.txt.gz");
+
+    meta.covfile.Add("tests/raremetal/test_tut_rm/inputs/STUDY1.QT1.singlevar.cov.txt.gz");
+    meta.covfile.Add("tests/raremetal/test_tut_rm/inputs/STUDY2.QT1.singlevar.cov.txt.gz");
+
+    GroupFromAnnotation group;
+    meta.PoolSummaryStat(group);
+
+    // This is actually an error in the tutorial dataset, where the alt allele is missing and the row is read incorrectly.
+    // It ends up being convenient, though, because it creates a record with a very extremely small p-value.
+    // The :432 is because raremetal ends up shifting the entire row left by 1 because of the missing alt entry.
+    int idx = meta.SNPmaf_name.Find("9:494428375:G:432");
+
+    String snp_name = meta.SNPmaf_name[idx];
+    StringArray tmp;
+    tmp.AddTokens(snp_name, ":");
+    String SNPname_noallele = tmp[0] + ":" + tmp[1];
+    int N = meta.SNP_effect_N[idx];
+    double U = meta.SNPstat.Double(SNPname_noallele);
+    double V = meta.SNP_Vstat.Double(SNPname_noallele);
+
+    SingleVariantResult result(U, V, N);
+    REQUIRE(result.effSize == Approx(1074.71));
+    REQUIRE(result.log_pvalue == Approx(1727.694));
+  }
+}
+
 TEST_CASE("Heterogeneity statistics") {
   SECTION("Should be correct on simple example") {
     Meta meta;
